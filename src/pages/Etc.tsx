15f29f797aae403933ed3f8c1c3ff319237f31ad
iooz <67130710+iooz@users.noreply.github.com>
2023-06-09 14:53:29 +0700

    Update Etc.tsx

--- src/pages/Etc.tsx
+++ src/pages/Etc.tsx
@@ -10,35 +10,15 @@ const Etc = () => (
     <h1>/etc</h1>
     <h2>Thanks</h2>
     <p>
-      Thanks for visiting. I hope you enjoyed your stay. The source code for this website is
+      Thanks for visiting and thanks for phineas for making this website, dont sue me. I hope you enjoyed your stay. The source code for this website is
       available at <a href={'https://github.com/phineas/phineas.io'}>phineas/phineas.io</a>
     </p>
     <h2>Contact</h2>
     <p>
-      I'm most responsive through Twitter DMs, you can{' '}
-      <a href={'https://twitter.com/messages/compose?recipient_id=102583719'}>click here</a> to DM
-      me on Twitter.
+      I'm most responsive through Instagram DMs, you can{' '}
+      <a href={'https://www.instagram.com/powerlessss_/'}>click here</a> to DM
+      me.
     </p>
-    <p>
-      If your inquiry is related to a network I manage or control (e.g. AS952 or AS399531), please
-      send an email to my email listed on the respective WHOIS contact. If it's an emergency, you
-      can email <a href="mailto:ops@sakuraji.ma">ops@sakuraji.ma</a>.
-    </p>
-    <h2>Other</h2>
-    <ul>
-      <li>
-        <Link to={'/presence'}>/presence</Link>
-      </li>
-      <li>
-        <a href={'https://phin.vc'}>phin.vc</a>
-      </li>
-      <li>
-        PGP Fingerprint: <code>D740 E413 3A74 815E F5B6 F11B AF8B EE0C CF1D 0E46</code>
-      </li>
-      <li>
-        Primary ETH Address: <code>phin.eth (0xd3AbB464df46568d1E72Fa5F156F05458f1AB26e)</code>
-      </li>
-    </ul>
   </PageWrapper>
 );
 
